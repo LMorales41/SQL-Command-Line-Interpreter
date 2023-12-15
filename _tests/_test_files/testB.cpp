06bf90f0cf95f5dddd949db6f5cb86a834db73e0
lmorales-41 <lmorales41@go.pasadena.edu>
2023-12-15 09:09:19 -0800

    desperation push

--- _tests/_test_files/testB.cpp
+++ _tests/_test_files/testB.cpp
@@ -52,37 +52,76 @@ bool test_sql(bool debug = false)
   Table books;
   Table orders;
   Table employees;
-  vector<Table> holding = {students, books, orders, employees};
-  vectorstr commandlines = 
-  {
-    "create table students fields id name age",
-    "make table books fields isbn title author price",
-    "create table orders fields \"order_id\" quantity",
-    "make table employees fields \"employee_id\" name salary"
-  };
-  for (int i = 0; i < commandlines.size(); i++)
-  {
-    holding[i] = sql.command(commandlines[i]);
-  }
 
   vectorstr insertlines = 
   {
+    "make table employees fields \"employee_id\" name salary",
     "insert employees values 101 \"John Doe\" 50000",
     "insert into employees values 102 \"Jane Smith\" 60000",
     "insert into employees values 103 \"Bob Johnson\" 55000",
-    "insert into employees values 104 \"Alice Williams\" 70000"
+    "create table students fields id name age",
+    "insert into employees values 104 \"Alice Williams\" 70000",
+    "insert employees values 105 \"Michael Johnson\" 55000",
+    "insert into employees values 106 \"Emily Davis\" 62000",
+    "insert employees values 107 \"Daniel White\" 48000",
+    "make table books fields isbn title author price",
+    "insert employees values 108 \"Olivia Martin\" 58000",
+    "insert employees values 109 \"Matthew Turner\" 65000",
+    "insert employees values 110 \"Sophia Harris\" 53000",
+    "create table orders fields \"order_id\" quantity",
+    "insert employees values 111 \"David Anderson\" 70000",
+    "insert employees values 112 \"Emma Taylor\" 48000",
+    "insert employees values 113 \"Christopher Lee\" 72000",
+    "insert into employees values 114 \"Isabella Wright\" 55000",
+    "insert into employees values 115 \"Ethan Martinez\" 59000",
+    "insert employees values 116 \"Ava Rodriguez\" 67000",
+    "insert employees values 117 \"Mia Clark\" 51000",
+    "insert employees values 118 \"James Baker\" 63000",
+    "insert employees values 119 \"Sophie Garcia\" 50000",
+    "insert employees values 120 \"Logan Smith\" 55000",
+    "insert employees values 121 \"Grace Walker\" 60000",
+    "insert employees values 122 \"Jackson Wright\" 54000",
+    "insert employees values 123 \"Chloe Davis\" 67000",
+    "insert employees values 124 \"Lucas Taylor\" 52000",
+    "insert employees values 125 \"Avery Turner\" 48000",
+    "insert employees values 126 \"Zoe Wilson\" 55000",
+    "insert employees values 127 \"Caleb Reed\" 71000",
+    "insert employees values 128 \"Nora Martin\" 59000",
+    "insert employees values 129 \"Henry Phillips\" 64000",
+    "insert employees values 130 \"Lily Moore\" 53000"
+
   };
-  for (int i =0 ; i < insertlines.size(); i++)
+  employees = sql.command(insertlines[0]);
+  for (int i =1 ; i < insertlines.size(); i++)
   {
+    if (i == 4 )
+    {
+      students = sql.command(insertlines[i]);
+      i++;
+    }
+    if (i == 9)
+    {
+      books = sql.command(insertlines[i]);
+      i++;
+    }
+    if (i == 13)
+    {
+      orders = sql.command(insertlines[i]);
+      i++;
+    }
     sql.command(insertlines[i]);
   }
+  cout << employees << endl;
+  cout << students << endl;
+  cout << orders << endl;
+  cout << books << endl;
 
-  for (int i =0 ; i < holding.size(); i++)
-  {
-    cout << holding[i] << endl;
-  }
+
   return true;
 }
+
+
+
 
 TEST(TEST_STUB, TestStub) {
   
