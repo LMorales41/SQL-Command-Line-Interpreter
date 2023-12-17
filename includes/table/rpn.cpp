e5a2181d300fe9dd96d506c7f4e27264254cbb0e
lmorales-41 <lmorales41@go.pasadena.edu>
2023-12-16 20:01:57 -0800

    fixed empty set bug

--- includes/table/rpn.cpp
+++ includes/table/rpn.cpp
@@ -105,10 +105,19 @@ void RPN::do_rpn_thing(Map <string, long>& fieldMap, vector<MMap<string, long>>&
                 if (l1.size() == 0)
                 {
                     l3 = l2;
+                    temp = new Solution(l3);
+                    solutionholder.push(temp);
                 }
                 else if (l2.size() == 0)
                 {
                     l3 = l1;
+                    temp = new Solution(l3);
+                    solutionholder.push(temp);
+                }
+                else if (l1.size()==0 && l2.size() == 0)
+                {
+                    temp = new Solution(l3);
+                    solutionholder.push(temp);
                 }
                 else
                 { 
@@ -116,13 +125,14 @@ void RPN::do_rpn_thing(Map <string, long>& fieldMap, vector<MMap<string, long>>&
                     {
                         l3.push_back(l2[z]);
                     }
-                }
-                sort(l3.begin(), l3.end());
-                auto last = std::unique(l3.begin(), l3.end()); //last will be itr where the dups are pushed to
-                l3.erase(last, l3.end()); //erases starting at last to end of vector
+                    sort(l3.begin(), l3.end());
+                    auto last = std::unique(l3.begin(), l3.end()); //last will be itr where the dups are pushed to
+                    l3.erase(last, l3.end()); //erases starting at last to end of vector
 
-                temp = new Solution (l3);
-                solutionholder.push(temp);
+                    temp = new Solution (l3);
+                    solutionholder.push(temp);
+                }
+
 
             }
         }
